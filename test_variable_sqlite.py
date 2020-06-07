90987f7fd07130b22d7ff00c77818d85a9cfa547
alicezou <alicezou26@gmail.com>
2020-06-07 11:52:19 -0400

    Variable Done

---
+++ test_variable_sqlite.py
@@ -0,0 +1,65 @@
+import pickle
+
+from sqlalchemy import Column, Integer, Unicode, UnicodeText, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import kgpl
+import KNPSStore
+engine = create_engine('sqlite:///KGPLData.db', echo=False)
+
+Session = sessionmaker(bind=engine)
+s = Session()
+
+if __name__ == '__main__':
+    val1 = kgpl.KGPLStr("val1")
+    val2 = kgpl.KGPLStr("val2")
+    var1 = kgpl.KGPLVariable(val1.id)
+    var2 = kgpl.KGPLVariable(val2.id)
+    print("+++")
+    print(val1.__repr__())
+    print("+++")
+    print(val2.__repr__())
+    print("+++")
+    print(var1.__repr__())
+    print("+++")
+    print(var2.__repr__())
+
+    var1.registerVariable()
+    var2.registerVariable()
+    # print(var1.__repr__())
+    # print(var2.__repr__())
+    var1.reassign(val2.id)
+    # print(val1.__repr__())
+    # print(val2.__repr__())
+    print("+++")
+    print(var1.__repr__())
+    print("+++")
+    print(var2.__repr__())
+
+    store = KNPSStore.KNPSStore('http://lasagna.eecs.umich.edu:8080')
+    var3 = store.GetVariable("V2")
+    print("+++")
+    print(var3.__repr__())
+    var4 = store.GetVariable("V1")
+    print("+++")
+    print(var4.__repr__())
+
+    # var1 = kgpl.KGPLVariable("var2-uuid")
+
+
+
+    # print(var1.__repr__())
+    # var1.registerVariable()
+
+
+
+    # test_store = KNPSStore.KNPSStore(None)
+    # # test_store.GetVariable("V2")
+    #
+    # print(var1)
+    # print(var1.timestamp)
+    # var1.reassign(val2.id)
+    #
+    # print(var1)
+    # print(var1.timestamp)
