c6f65b70cd5bf304aedc1f6e2e356e544653b2f7
wangyitong9 <wendy991213@sjtu.edu.cn>
2020-06-29 22:47:10 -0400

    fix utils

--- utils.py
+++ utils.py
@@ -18,16 +18,16 @@ import time
 
 time1 = time.time()
 label_desc_dict = {}
-with open('/data/wikidata/label_desc.txt') as json_file:
+with open('label_desc.txt', 'rt') as json_file:
     label_desc_dict = json.load(json_file)
 print(time.time()-time1)
 
-label_desc_dict = {}
+# label_desc_dict = {}
 # with open('/data/wikidata/label_desc.txt') as json_file:
 #     label_desc_dict = json.load(json_file)
 
-with open('temp.txt') as json_file:
-    label_desc_dict = json.load(json_file)
+# with open('temp.txt') as json_file:
+#    label_desc_dict = json.load(json_file)
 
 def parse(source):
     """ Parse a raw user query."""
