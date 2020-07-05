745eb10da9e5b92c38e3421e4fa5d812ba4c92d0
Dinghao Shen <jackyy@umich.edu>
2020-07-04 22:25:30 -0400

    Modification on query

--- query.py
+++ query.py
@@ -11,16 +11,17 @@ class IR:
             entity_id: the entity ID.
             focus: the part of the IR that is built. 
                 E.g., For Q30.GDP, we build an IR for Q30 with only GDP property data fetched.
-        
+
         Attributes:
             type (str): item / property. Others are not supported yet.
             id (str)
-            focus (str): The property_id of the IT 
+            focus (str): The property_id of the IT
             desc (str)
             aliases (List[str])
             properties (Dict): Keys are property_id, values are DataFrame
-    """ 
-    def __init__(self, entity_id: str, KG: str, focus: str=None):
+    """
+
+    def __init__(self, entity_id: str, KG: str, focus: str = None):
         if KG.lower() != "wikidata":
             raise ValueError("Unsupported KG: {}".format(KG))
         self.KG = KG
@@ -36,7 +37,7 @@ class IR:
         else:
             raise ValueError("Unsupported entity type with id = {}".format(entity_id))
         self._generate_IR_()
-        
+
     def _generate_IR_(self):
         entity_obj = get_entity(self.id)
         if 'missing' in entity_obj.keys():
@@ -58,6 +59,7 @@ class IR:
                 continue
             data_df = None
             dictionary = None
+            count_snak = 0
             for snak in snaks:
                 mainsnak = snak.get("mainsnak")
                 qualifiers = snak.get("qualifiers")
@@ -72,26 +74,32 @@ class IR:
                 #
                 # Assume no overlapping keys in qualifiers_maping and value_mapping
                 #
-                assert(set(value_mapping.keys()) != set(qualifiers_mapping.keys()))
+                assert (set(value_mapping.keys()) != set(qualifiers_mapping.keys()))
                 value_mapping.update(qualifiers_mapping)
                 #
                 # Add prefix to all keys
                 #
-                # key_prefix = self.label + "." + 
+                # key_prefix = self.label + "." +
                 # value_mapping = {key_prefix+k: v for k, v in value_mapping.items()}
 
-                ###
+                ##
                 # if data_df is None:
                 #     data_df = pd.DataFrame.from_records([value_mapping])
                 # else:
                 #     data_df = data_df.append(value_mapping, ignore_index=True, sort=True)
-                ###
-
+                ##
                 if dictionary is None:
                     dictionary = value_mapping
                 else:
-                    dictionary = dictionary.update(value_mapping)
-
+                    if (count_snak == 0):
+                        temp = dictionary
+                        dictionary = {}
+                        dictionary[0] = temp
+                        dictionary[1] = value_mapping
+                        count_snak = 2
+                    else:
+                        dictionary[count_snak] = value_mapping
+                        count_snak += 1
                 # print(data_df)
             #
             # Open question: do we want to include property labels in keys?
@@ -108,7 +116,6 @@ class IR:
                 self.properties[property_id] = dictionary
             else:
                 self.properties[property_id] = dictionary
-
 
     def __getitem__(self, key):
         return self.properties.get(key)
@@ -141,7 +148,7 @@ class Query:
         # Generates IRs
         #
         self._data_preprocess_()
-    
+
     def _parse_user_query_(self):
         """
         Fow now: assume the user uses a dataset from seaborn by passing the dataset name
@@ -163,7 +170,7 @@ class Query:
 
             Args:
                 KG_params: A list of expressions referring to KG data.
-                        -- An entity (whether the user refers to it directly, as in Q76, or via an expression, as in Q1048.P19) 
+                        -- An entity (whether the user refers to it directly, as in Q76, or via an expression, as in Q1048.P19)
                         -- Others (E.g., Q76.P569)
         """
         KG_params = {}
@@ -195,6 +202,6 @@ class Query:
         else:
             raise ValueError("Unsupported KG: {}".format(KG))
         self.KG_params = KG_params
-    
+
     def __getitem__(self, key):
         return self.KG_params.get(key)
