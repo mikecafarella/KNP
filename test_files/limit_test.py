b1dab05727a1db1e0647defbf50b58e41448a9a7
wangyitong9 <wendy991213@sjtu.edu.cn>
2020-11-03 17:48:35 -0500

    dialog topleft and same font for val and var

--- test_files/limit_test.py
+++ test_files/limit_test.py
@@ -1,15 +1,16 @@
-import kgpl
+import kgpl_client as kgpl
 import random
 total = 0
 server_url = "http://127.0.0.1:5000/val/"
-for i in range(0, 100):
-    user_name = "user" + str(i)
+for i in range(0, 20):
+    user_name = "user ." + str(i)
     for y in range(0, random.randint(1, 4)):
         d = []
         if (total > 5):
             for j in range(0, 3):
                 d.append(server_url + str(random.randint(1, total - 1)))
-        kgpl.value(1, "comment", user=user_name, dependency=d)
+        a = kgpl.value(1, "comment", user=user_name, dependency=d)
+        kgpl.variable(a, "comment_var", user=user_name)
         total += 1
 
 a = kgpl.value(123, "ss")
