11ef028fe3e687a0fcd3728e92bc13e3c08192c1
Dinghao Shen <jackyy@umich.edu>
2020-11-05 00:11:43 +0800

    Add feature of extend function to multiple parameters

--- func_lib.py
+++ func_lib.py
@@ -1,4 +1,6 @@
 import datetime
+import requests
+from bs4 import BeautifulSoup
 
 func_list = []
 
@@ -17,10 +19,38 @@ def temp1(name: str):
 def temp2(name: str):
     return name.split(' ')[-1]
 
-def temp3(birth):
+def temp3(birth, death):
     if birth == 'NA':
         return 'NA'
-    date_time_obj = datetime.datetime.strptime(birth[:4], '%Y')
-    return datetime.datetime.now().year - date_time_obj.year
+    if death == 'NA':  
+        date_time_obj = datetime.datetime.strptime(birth[:4], '%Y')
+        return datetime.datetime.now().year - date_time_obj.year
+    else:
+        return datetime.datetime.strptime(death[:4], '%Y').year - datetime.datetime.strptime(birth[:4], '%Y').year
+    
+def temp4(x):
+    url = 'https://en.wikipedia.org/wiki/'
+    for word in x.split(' '):
+        url += (word + '_')
+    url = url[:-1]
+    s = requests.session()
+    r = s.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    results = soup.find(id='bodyContent')
+    results = results.find(id='mw-content-text')
+    results = results.find_all('p')
+    if len(results) > 1:
+        return results[0].text.split('.')[0]
+    return results[0].text.split('.')[0]
 
-func_list = [temp0, temp1, temp2, temp3]
+def temp5(x):
+    if x == 'NA':
+        return 'NA'
+    url = 'https://www.google.com/maps/place/'
+    for word in x.split(' '):
+        url += (word + '%20')
+    url = url[:-3]
+    # r = requests.get(url)
+    return url
+
+func_list = [temp0, temp1, temp2, temp3, temp4, temp5]
