1e0fcaf441d53b901b41afd22800e56d01b84de6
Mike Anderson <mrander@gmail.com>
2020-12-08 16:11:36 -0500

    Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../client/knps'))
 sys.path.insert(0, os.path.abspath('../server/server'))
 sys.path.insert(0, os.path.abspath('../'))
@@ -30,9 +32,11 @@ author = 'KNPS Team'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [	
+	"sphinx_rtd_theme",
+	"sphinx.ext.autodoc",
+]
 
-extensions.append('sphinx.ext.autodoc')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
