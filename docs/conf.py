3b5f1be76fa9387ab5adc03bb31d54c5ac98c541
Mike Anderson <mrander@gmail.com>
2020-12-09 10:00:20 -0500

    Docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = 'KNPS Team'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [	
+extensions = [
 	"sphinx_rtd_theme",
 	"sphinx.ext.autodoc",
 ]
@@ -46,6 +46,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index' 
 
 # -- Options for HTML output -------------------------------------------------
 
