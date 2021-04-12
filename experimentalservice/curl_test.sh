5c7bc9bc5068975b67690b566c520dccaae1fa8b
alicezou <alicezou@umich.edu>
2021-04-11 23:50:57 -0400

    delete garbage

--- experimentalservice/curl_test.sh
+++
@@ -1,7 +0,0 @@
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:9200/)
-if [ $response -eq 200 ]; 
-then 
-    echo 'done'
-else
-    echo 'pending'
-fi
--- experimentalservice/test_script.sh
+++
@@ -1,5 +0,0 @@
-value=`cat recent_start_script_npm`
-echo "$value"
-# kill -INT -626
-value=`cat recent_start_script_elastic`
-echo "$value"
