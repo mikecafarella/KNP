9702e852aea94267a6fd88d5c3f63abafa2ecd10
Michael Cafarella <michjc@31-36-42.wireless.csail.mit.edu>
2021-11-30 13:41:31 -0500

    Fix username display

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -167,7 +167,7 @@ const Blog : React.FC<{dobj: UserProps}> = ({dobj}) => {
                   <Table.Row key={nd.fileid} isSelectable onSelect={() => Router.push(`/knownlocation/${nd.uuid}`)}>
                     <Table.TextCell>{nd.filename} {nd.latest==1 ? '' :
                     '(Obsolete)'} </Table.TextCell>
-                    <Table.TextCell>{nd.owner}</Table.TextCell>
+                    <Table.TextCell>{nd.username}</Table.TextCell>
                     <Table.TextCell>{nd.modified}</Table.TextCell>                                        
                   </Table.Row>
 
