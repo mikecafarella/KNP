f8b1b0f272c8f58530b53d6ce167e2d2c388d4d6
syang98 <syang199@gmail.com>
2022-01-10 21:19:13 -0500

    Uncomment lines in boot_strap demo.sh

--- service/bootstrap_demo.sh
+++ service/bootstrap_demo.sh
@@ -1,10 +1,10 @@
 # python3 delete_es_index.py
 cd examples/covid
 ./run_all.sh
-# cd ../cord
-# python3 step1.py
-# python3 step2.py
-# cd ../csv
-# python3 step1.py
-# cd ../scales
-# python3 step1.py
+cd ../cord
+python3 step1.py
+python3 step2.py
+cd ../csv
+python3 step1.py
+cd ../scales
+python3 step1.py
