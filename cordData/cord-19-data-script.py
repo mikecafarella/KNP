927c83fbc4ee16c3179d630d00dccac65acbed66
KYue17 <kevinyue@mit.edu>
2022-01-11 17:52:49 -0800

    changed from one date to all dates

--- cordData/cord-19-data-script.py
+++ cordData/cord-19-data-script.py
@@ -11,12 +11,27 @@ import shutil
 from bs4 import BeautifulSoup
 import lxml
 
-#Extract latest CORD-19 release date
+#TODO: s3 bucket credentials
+# aws_credentials = open("aws_keys.json", 'r')
+# aws_keys = json.load(aws_credentials)
+# ACCESS_KEY_ID = aws_keys['access_key_id']
+# SECRET_ACCESS_KEY = aws_keys['secret_access_key']
+# aws_credentials.close()
+# os.environ["AWS_DEFAULT_REGION"] = 'us-east-1'
+# os.environ["AWS_ACCESS_KEY_ID"] = ACCESS_KEY_ID
+# os.environ["AWS_SECRET_ACCESS_KEY"] = SECRET_ACCESS_KEY
+# BUCKET_NAME = 'knps-data'
+# s3 = boto3.resource('s3')
+
+#Extract CORD-10 release dates
 source = requests.get('https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html').text
 soup = BeautifulSoup(source, 'lxml')
 table = soup.find('table')
-latest_entry = table.find_all('tr')[1]
-date = latest_entry.td.text
+entries = table.find_all('tr')[1:]
+dates = map(lambda entry : entry.td.text, entries)
+print(list(dates))
+
+#Iterate over all dates and do this: 
 
 #Get url for CORD-19 Paper metadata
 s3bucket_url = "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/"
@@ -33,3 +48,8 @@ else:
   with open("metadata.csv", 'wb') as f:
     shutil.copyfileobj(res.raw, f)
   print("Download complete.")
+
+#Upload to s3
+
+
+#Download from s3
